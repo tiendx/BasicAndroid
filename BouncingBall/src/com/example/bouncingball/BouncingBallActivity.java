5bc43ff8bc01d091c10aef25af1bfed4cc01908b
Dang Xuan Tien <dangtienhus@gmail.com>
2013-09-26 01:00:16 +0700

    version first

---
+++ BouncingBall/src/com/example/bouncingball/BouncingBallActivity.java
@@ -0,0 +1,15 @@
+package com.example.bouncingball;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+  
+public class BouncingBallActivity extends Activity {
+   /** Called when the activity is first created. */
+   @Override
+   public void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      View bouncingBallView = new BouncingBallView(this);
+      setContentView(bouncingBallView);
+   }
+}
